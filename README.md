4e715c5a81d3f4befaa74aeef9dceea2e2d9505c
Northern-Bear <88998789+Northern-Bear@users.noreply.github.com>
2021-08-16 13:55:44 -0600

    fix typo in README add quote array

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A Codecademy portfolio project written using JavaScript. This simple program
 displays a random message each time the program is run.
 
-Norther-Bear
+Northern-Bear
---
+++ mixed-message.js
@@ -0,0 +1,53 @@
+// array of movieQuote objects to iterate through.
+const movieQuotes = [
+    {
+        quote: 'To infinity, and beyond!',
+        character: 'Buzz Lightyear',
+        movie: 'Toy Story'
+    },
+    {
+        quote: 'Cargo and ship destroyed. I should reach the frontier in about 6 weeks. With a little luck, the network will pick me up. This is Ripley, last survivor of The Nostromo, signing off.',
+        character: 'Ripley',
+        movie: 'Alien'
+    },
+    {
+        quote: 'Listen! And understand! That terminator is out there. It can\'t be bargained with! It can\'t be reasoned with! It doesn\'t feel pity, or remorse, or fear. And it absolutely will not stop, ever, until you are dead!',
+        character: 'Kyle Reese',
+        movie: 'Terminator'
+    },
+    {
+        quote: 'Hello, my name is Inigo Montoya. You killed my father: prepare to die.',
+        character: 'Inigo Montoya',
+        movie: 'Princess Bride'
+    },
+    {
+        quote: 'Mama always said life was like a box a chocolates, never know what you\'re gonna get.',
+        character: 'Forest Gump',
+        movie: 'Forest Gump'
+    },
+    {
+        quote: 'If I\'m not back in five minutes... just wait longer.',
+        character: 'Ace Ventura',
+        movie: 'Ace Ventura: When Nature Calls'
+    },
+    {
+        quote: 'Yea, I called her up. She gave me a bunch of crap about me not listening to her enough or something. I don\'t know I wasn\'t really paying attention.',
+        character: 'Harry Dunne',
+        movie: 'Dumb and Dumber'
+    },
+    {
+        quote: 'I make it a rule, never get involved with possessed people......actually, it\'s more of a guideline than a rule.',
+        character: 'Doctor Peter Venkman',
+        movie: 'Ghostbusters'
+    },
+    {
+        quote: 'I\'ve fallen for you like a blind roofer.',
+        character: 'Topper Harley',
+        movie: 'Hot Shots'
+    },
+    {
+        quote: 'Snakes. Why did it have to be snakes?',
+        character: 'Indiana Jones',
+        movie: 'Raiders of the Lost Ark'
+    }
+];
